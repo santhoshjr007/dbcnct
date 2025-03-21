e2d6f981b1c84b6e087b855de4559b8b5d6d5f1f
santhoshjr007 <santhosh.r@kalvium.community>
2025-03-21 09:15:22 +0530

    done

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const { resolve } = require('path');
-
+const dotenv=require('dotenv').config();
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
